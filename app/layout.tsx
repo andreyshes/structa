c1ede9e43df41b55cd046b7b4b63251e35f9b9f7
Andrey Shestopal <andreyshestopal@Andreys-MacBook-Pro.local>
2026-06-08 15:01:35 -0700

    Add Google Business Profile URL to sameAs schema

--- app/layout.tsx
+++ app/layout.tsx
@@ -113,7 +113,9 @@ export default function RootLayout({
 									"numberOfEmployees": { "@type": "QuantitativeValue", "value": "1-10" },
 									"foundingDate": "2022",
 									"slogan": "5-Star Rated. Licensed, Bonded & Insured. We Show Up.",
-									"sameAs": [],
+									"sameAs": [
+										"https://www.google.com/maps/place/NORBILT/@45.6933319,-122.5403396,11z/data=!3m1!4b1!4m6!3m5!1s0x6ba3be3e3106abb:0x1cb70e756db2c95b!8m2!3d45.6933319!4d-122.5403396!16s%2Fg%2F11yzz5_myb",
+									],
 								},
 								{
 									"@type": "GeneralContractor",
